45a660ad36029cca20a0b23b3205beed47202fdf
Anthony Kirby <Anthony.Kirby@nominet.uk>
2019-01-07 13:23:44 +0000

    renamed

--- static_build/build_book.sh
+++ static_build/build_book.sh
@@ -6,10 +6,10 @@
 echo "exporting source"
 rm -rf exported_source/ || exit 1
 mkdir -p exported_source/ || exit 1
-(cd .. && git checkout-index -a --prefix=docker_build/exported_source/) || exit 1
+(cd .. && git checkout-index -a --prefix=static_build/exported_source/) || exit 1
 
 # but we don't want (or need) this build directory
-rm -rf exported_source/docker_build
+rm -rf exported_source/static_build
 
 echo "running docker build"
 docker run --rm  --volume=`pwd`/exported_source:/shared:z microbit-mkdocs
